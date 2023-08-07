53ffd3b970ffeea3df3601db338b7b9a8f896631
gerardo <tangogerard890@gmail.com>
2023-08-07 16:57:01 +0200

    function keyplay

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
-
+function keyPlay(event) {
+  let key = event.target;
+  key.style.backgroundColor='yellow';
+}
 
 // Write a named function with event handler properties
 
